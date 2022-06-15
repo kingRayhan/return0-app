3b2080191b7ed335064b854cca3e78769ba62f5c
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2022-06-16 01:08:38 +0600

    color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#3465A4",
+          blue: "#3465A4",
+          green: "#4E9A06",
+          dark: "#0F172A",
+        },
+      },
+    },
   },
   plugins: [],
 };
